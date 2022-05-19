b0425066f269916974b9e6e996ad349dd4e6c556
Francisco Santos <francisco.phb04@gmail.com>
2022-05-19 16:37:21 -0300

    Atualização no JS para o Menu-mobile
    Cria um código que abre o menu e fecha o menu. Mais alguns comentários no código.

--- JS/navbar-menu.js
+++ JS/navbar-menu.js
@@ -2,5 +2,14 @@ var menuBar = document.querySelector('.menu-mobile i')
 menuBar.addEventListener('click',clicado)
 function clicado(){
     let menuMobile = document.querySelector('.menu-mobile ul')
-    menuMobile.classList.add('show-menu')
+    
+    //a condição abaixo faz com que seja criado a opção de remover o menu.
+
+    if(menuMobile.classList.contains(`show-menu`)){
+
+        menuMobile.classList.remove(`show-menu`)
+
+    }else{
+        menuMobile.classList.add('show-menu')
+    }
 }
